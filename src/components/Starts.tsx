e66056d4af2b5f357e9f3bbbb4771a990ee4a5f9
Royer Bonifacio <royer.boni13@gmail.com>
2025-09-20 00:25:13 -0500

    Add save contact

--- src/components/Starts.tsx
+++ src/components/Starts.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 import { FaRegStar, FaStar } from 'react-icons/fa';
 import FormReview from './FormReview';
 
-
 function Starts() {
   const [hovered, setHovered] = useState<number | null>(null);
   const [rating, setRating] = useState<number | null>(null);
   const showForm = rating !== null && rating <= 3;
   const total = 5;
-  const reviewLink = 'https://www.yelp.com/biz/h-and-a-tree-brothers-orlando?utm_campaign=www_business_share_popup&utm_medium=copy_link&utm_source=(direct)';
+  const reviewLink =
+    'https://www.yelp.com/biz/h-and-a-tree-brothers-orlando?utm_campaign=www_business_share_popup&utm_medium=copy_link&utm_source=(direct)';
 
   // Move any client-only side effects (redirect) into useEffect so SSR won't break.
   useEffect(() => {
@@ -50,7 +50,7 @@ function Starts() {
           );
         })}
       </div>
-      {showForm && <FormReview/>}
+      {showForm && <FormReview />}
     </>
   );
 }
